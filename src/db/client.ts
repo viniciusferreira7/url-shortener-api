5f455c17973d99c46d42b37817918d34294a1203
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-10-05 08:43:47 -0300

    chore: create action to run on github actions

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,4 +1,5 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { env } from '@/env';
+import { schema } from './schema';
 
 export const db = drizzle(env.DATABASE_URL, { schema, casing: 'snake_case' });
