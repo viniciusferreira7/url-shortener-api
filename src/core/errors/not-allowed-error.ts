0388e66cd1379ff9828e48399e4c6609505c084f
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-11-02 11:39:41 -0300

    feat: create use case error

--- src/core/errors/not-allowed-error.ts
+++ src/core/errors/not-allowed-error.ts
@@ -1,7 +1,7 @@
-import type { UseCaseError } from "@/core/errors/use-case-error";
+import type { UseCaseError } from './use-case-error';
 
 export class NotAllowedError extends Error implements UseCaseError {
 	constructor() {
-		super("Not allowed");
+		super('Not allowed');
 	}
 }
