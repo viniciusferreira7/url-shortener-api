92ed94da677c9171bd84bf02eed1015871986b1a
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-11 10:17:57 -0300

    feat: add factory for GetRankingUseCase
    Implements dependency injection factory for GetRankingUseCase.

---
+++ src/infra/factories/make-get-ranking-use-case.ts
@@ -0,0 +1,13 @@
+import { GetRankingUseCase } from '@/domain/url-shortening/application/use-cases/get-ranking';
+import { drizzleDb } from '../db/drizzle/client';
+import { DrizzleUrlsRepository } from '../db/drizzle/repositories/drizzle-url-repository';
+import { RedisAnalysisRepository } from '../db/redisClient/repositories/redisClient-analysis-repostitory';
+import { redisClient } from '../db/redisClient/client';
+import { env } from '../env';
+
+export function makeGetRankingUseCase(): GetRankingUseCase {
+  const analysisRepository = new RedisAnalysisRepository(redisClient, env);
+  const urlsRepository = new DrizzleUrlsRepository(drizzleDb, analysisRepository);
+
+  return new GetRankingUseCase(urlsRepository, analysisRepository);
+}
