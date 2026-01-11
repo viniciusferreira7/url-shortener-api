381cf4c931e85e5ee1a8080f0549aa26123d0dfd
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-11 10:17:47 -0300

    feat: add factory for LikeUrlUseCase
    Implements dependency injection factory for LikeUrlUseCase.

---
+++ src/infra/factories/make-like-url-use-case.ts
@@ -0,0 +1,15 @@
+import { LikeUrlUseCase } from '@/domain/url-shortening/application/use-cases/like-url';
+import { drizzleDb } from '../db/drizzle/client';
+import { DrizzleUrlsRepository } from '../db/drizzle/repositories/drizzle-url-repository';
+import { DrizzleUsersRepository } from '../db/drizzle/repositories/drizzle-users-repository';
+import { RedisAnalysisRepository } from '../db/redisClient/repositories/redisClient-analysis-repostitory';
+import { redisClient } from '../db/redisClient/client';
+import { env } from '../env';
+
+export function makeLikeUrlUseCase(): LikeUrlUseCase {
+  const analysisRepository = new RedisAnalysisRepository(redisClient, env);
+  const usersRepository = new DrizzleUsersRepository(drizzleDb);
+  const urlsRepository = new DrizzleUrlsRepository(drizzleDb, analysisRepository);
+
+  return new LikeUrlUseCase(usersRepository, urlsRepository);
+}
