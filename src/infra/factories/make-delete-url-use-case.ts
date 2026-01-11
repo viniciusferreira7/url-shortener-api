b0330915885e947ea650e6b52ef1cb7f1d8f97e4
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-11 10:17:46 -0300

    feat: add factory for DeleteUrlUseCase
    Implements dependency injection factory for DeleteUrlUseCase.

---
+++ src/infra/factories/make-delete-url-use-case.ts
@@ -0,0 +1,15 @@
+import { DeleteUrlUseCase } from '@/domain/url-shortening/application/use-cases/delete-url';
+import { drizzleDb } from '../db/drizzle/client';
+import { DrizzleUrlsRepository } from '../db/drizzle/repositories/drizzle-url-repository';
+import { DrizzleUsersRepository } from '../db/drizzle/repositories/drizzle-users-repository';
+import { RedisAnalysisRepository } from '../db/redis/repositories/redis-analysis-repostitory';
+import { redisClient } from '../db/redis/client';
+import { env } from '../env';
+
+export function makeDeleteUrlUseCase(): DeleteUrlUseCase {
+  const analysisRepository = new RedisAnalysisRepository(redisClient, env);
+  const usersRepository = new DrizzleUsersRepository(drizzleDb);
+  const urlsRepository = new DrizzleUrlsRepository(drizzleDb, analysisRepository);
+
+  return new DeleteUrlUseCase(usersRepository, urlsRepository);
+}
