99a989f84ee87c1248d1337707d7ffd97f24ed6e
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-11 10:17:46 -0300

    feat: add factory for GetUrlByCodeUseCase
    Implements dependency injection factory for GetUrlByCodeUseCase.

---
+++ src/infra/factories/make-get-url-by-code-use-case.ts
@@ -0,0 +1,13 @@
+import { GetUrlByCodeUseCase } from '@/domain/url-shortening/application/use-cases/get-url-by-code';
+import { drizzleDb } from '../db/drizzle/client';
+import { DrizzleUrlsRepository } from '../db/drizzle/repositories/drizzle-url-repository';
+import { RedisAnalysisRepository } from '../db/redis/repositories/redis-analysis-repostitory';
+import { redisClient } from '../db/redis/client';
+import { env } from '../env';
+
+export function makeGetUrlByCodeUseCase(): GetUrlByCodeUseCase {
+  const analysisRepository = new RedisAnalysisRepository(redisClient, env);
+  const urlsRepository = new DrizzleUrlsRepository(drizzleDb, analysisRepository);
+
+  return new GetUrlByCodeUseCase(urlsRepository, analysisRepository);
+}
