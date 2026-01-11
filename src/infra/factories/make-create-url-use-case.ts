95941aec29130ce07764ef4559e9481a1708c21f
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-11 10:17:35 -0300

    feat: add factory for CreateUrlUseCase
    Implements dependency injection factory for CreateUrlUseCase with
    all required dependencies (repositories, code generator).

---
+++ src/infra/factories/make-create-url-use-case.ts
@@ -0,0 +1,22 @@
+import { CreateUrlUseCase } from '@/domain/url-shortening/application/use-cases/create-url';
+import { drizzleDb } from '../db/drizzle/client';
+import { DrizzleUrlsRepository } from '../db/drizzle/repositories/drizzle-url-repository';
+import { DrizzleUsersRepository } from '../db/drizzle/repositories/drizzle-users-repository';
+import { RedisAnalysisRepository } from '../db/redis/repositories/redis-analysis-repostitory';
+import { redisClient } from '../db/redis/client';
+import { env } from '../env';
+import { Base62UrlCodeGenerator } from '../url-code/base62-url-code-generator';
+
+export function makeCreateUrlUseCase(): CreateUrlUseCase {
+  const analysisRepository = new RedisAnalysisRepository(redisClient, env);
+  const usersRepository = new DrizzleUsersRepository(drizzleDb);
+  const urlsRepository = new DrizzleUrlsRepository(drizzleDb, analysisRepository);
+  const urlCodeGenerator = new Base62UrlCodeGenerator();
+
+  return new CreateUrlUseCase(
+    usersRepository,
+    urlsRepository,
+    analysisRepository,
+    urlCodeGenerator
+  );
+}
