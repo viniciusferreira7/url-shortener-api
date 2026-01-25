e9758a6c8f876beda3e284f96aa968c1fe132643
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-25 09:03:09 -0300

    feat: schema of url to use on response

---
+++ src/infra/http/docs/schemas/url-schema-response.ts
@@ -0,0 +1,23 @@
+import z from 'zod';
+
+export const urlSchemaResponse = z.object({
+  id: z.uuidv7().describe('ID of URL'),
+  author_id: z.uuidv7().describe('ID of author'),
+  name: z.string().describe('Name of URL'),
+  code: z
+    .string()
+    .describe(
+      'Code of URL, where it will used to redirect when request destination URL'
+    ),
+  description: z.string().describe('Description of URL').nullish(),
+  is_public: z
+    .boolean()
+    .describe(
+      'If set with true value URL can be viewed by another users, otherwise only author can see the URL'
+    ),
+  like: z.number().describe('Number likes tht URL got'),
+  score: z.number().min(0).describe('Number of time that URL was accessed'),
+  destination_url: z.url().describe('Destination URL is original URL'),
+  created_at: z.date().describe('Identifier when URL was created'),
+  updated_at: z.date().describe('Identifier when URL was updated').nullable(),
+});
