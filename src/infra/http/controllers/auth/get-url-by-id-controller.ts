11fc0dd1ac746a6ebfd301e7152d8ab7ca71702e
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-02-22 09:10:15 -0300

    feat: create controller to get url by id

---
+++ src/infra/http/controllers/auth/get-url-by-id-controller.ts
@@ -0,0 +1,47 @@
+import { Elysia } from 'elysia';
+import z from 'zod';
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error';
+import { makeGetUrlByIdUseCase } from '@/infra/factories/make-get-url-by-id-use-case';
+import { UrlPresenter } from '../../presenters/url-presenter';
+import { urlSchemaResponse } from '../../utils/schemas/url-schema-response';
+
+export const getUrlByIdController = new Elysia().get(
+  '/:id',
+  async ({ set, params }) => {
+    const useCase = makeGetUrlByIdUseCase();
+
+    const result = await useCase.execute({
+      urlId: params.id,
+    });
+
+    if (result.isLeft()) {
+      const error = result.value;
+
+      switch (error.constructor) {
+        case ResourceNotFoundError:
+          set.status = 404;
+          return { message: error.message };
+        default:
+          set.status = 400;
+          return { message: 'Bad request' };
+      }
+    }
+
+    return UrlPresenter.toHttp(result.value.url);
+  },
+  {
+    detail: {
+      summary: 'Redirect to destination URL by short code (Public)',
+      tags: ['URL'],
+    },
+    params: z.object({
+      id: z.string().describe('ID of URL'),
+    }),
+    response: {
+      200: urlSchemaResponse,
+      404: z.object({
+        message: z.string().describe('Resource not found'),
+      }),
+    },
+  }
+);
